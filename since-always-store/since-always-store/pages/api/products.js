f69e0fa2241b649aae297b20811798b00eeada55
coolguy3004 <coolguy3004@gmail.com>
2025-05-09 17:17:54 -0400

    Delete since-always-store/since-always-store directory

--- since-always-store/since-always-store/pages/api/products.js
+++
@@ -1,8 +0,0 @@
-
-export default async function handler(req, res) {
-  const result = await fetch('https://api.printful.com/products', {
-    headers: { Authorization: `Bearer ${process.env.PRINTFUL_API_KEY}` }
-  });
-  const data = await result.json();
-  res.status(200).json(data.result || []);
-}
